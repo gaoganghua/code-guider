b11ce56fe621937a2cdd1a21fc03a70a93cb7a99
gaoganghua <gaoganghua@gxb.io>
2019-01-05 22:40:55 +0800

    init files

---
+++ src/main/java/com/code/guider/CodeGuiderApplication.java
@@ -0,0 +1,14 @@
+package com.code.guider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGuiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGuiderApplication.class, args);
+    }
+
+}
+
